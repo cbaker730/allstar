17bc52b94feb947e0f8dfe15def5f4bc301a98a6
Chris Baker <bakercw@verizon.net>
2025-05-09 20:43:29 -0400

    Update allstar-autoconnect.sh
    Added check for internet connection

--- allstar-autoconnect.sh
+++ allstar-autoconnect.sh
@@ -5,19 +5,33 @@
 # @reboot /usr/local/sbin/allstar-autoconnect.sh
 
 # Save the following to /usr/local/sbin/allstar-autoconnect.sh
+
 #!/bin/bash
 
 count=0
 max_attempts=20
+check_asterisk=false
+check_internet=false
 
 sleep 60
+
 while [ $count -lt $max_attempts ]; do
-    if pgrep -f "sbin\/asterisk" > /dev/null; then
-        break
-    else
-        sleep 5
-        count=$((count + 1))
+
+    if ! $check_asterisk && pgrep -f "sbin\/asterisk" > /dev/null; then
+        check_asterisk=true
     fi
+
+    if ! $check_internet && ping -c 1 8.8.8.8 &> /dev/null; then
+        check_internet=true
+    fi
+
+    # If both conditions are met, exit the loop
+    if $check_asterisk && $check_internet; then
+        break
+    fi
+
+    sleep 5
+
 done
 
 # Example: /usr/sbin/asterisk -rx "rpt cmd 54321 ilink 3 55553"
